9ee639aefcad9722651a360aa4357492b7d68f2b
yamwoong <0206woong@naver.com>
2022-11-16 22:06:43 +0900

    second commit

--- Algorithm/src/chap01/Median.java
+++ Algorithm/src/chap01/Median.java
@@ -1,5 +1,37 @@
 package chap01;
+import java.util.Scanner;
 
 class Median{
-	stati
+	static int med3(int a, int b, int c) {
+		if(a >= b) {
+			if(b >= c) {
+				return b;
+			} else if(a <= c) {
+				return a;
+			} else {
+				return c;
+			}
+		} else if(a > c) {
+			return a;
+		} else if(b > c) {
+			return c;
+		} else {
+			return b;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner stdId = new Scanner(System.in);
+		
+		System.out.println("세 정수의 중앙값을 구합니다.");
+		System.out.print("a의 값: ");
+		int a = stdId.nextInt();		
+		System.out.print("b의 값: ");
+		int b = stdId.nextInt();		
+		System.out.print("c의 값: ");
+		int c = stdId.nextInt();
+		
+		System.out.println("중앙값은 " + med3(a, b, c) + "입니다.");
+		
+	}
 }
